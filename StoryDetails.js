193a2e6a367e1918e956cdbe929ef71444415922
girlyrobot <ctangsgaard@gmail.com>
2018-07-25 08:49:21 +0200

    added interruption categories and icons

--- StoryDetails.js
+++ StoryDetails.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { AppLoading, Font } from 'expo';
-import { FlatList, ListView, View } from 'react-native';
+import { FlatList, ListView, StyleSheet, View } from 'react-native';
 import {
 	  Button,
 	  Container,
 	  Header,
+		Icon,
 	  Content,
-	  Icon,
 	  List,
 	  ListItem,
 	  Text,
@@ -16,7 +16,7 @@ import {
 import Timestamp from 'react-timestamp';
 
 export default class StoryDetails extends React.Component {
-	
+
   static navigationOptions = ({ navigation }) => {
 	return {
 		title: 'Details for story: ' + navigation.getParam('story').data().name,
@@ -27,13 +27,13 @@ export default class StoryDetails extends React.Component {
   constructor(props) {
 	  super(props);
 	  this.story = props.navigation.getParam('story');
-	  
-	  this.state = {
+  	this.state = {
 		     started: this.story.data().startedOn !== undefined,
 		     startedOn: this.story.data().startedOn !== undefined ? this.story.data().startedOn.seconds : undefined,
 		     finished: this.story.data().finishedOn !== undefined,
 		     finishedOn: this.story.data().finishedOn !== undefined ? this.story.data().finishedOn.seconds : undefined,
 		     interruptions: this.story.data().interruptions !== undefined ? this.story.data().interruptions : [],
+				 interruptionCategories: this.story.data().interruptionCategories !== undefined ? this.story.data().interruptionCategories : [],
 		   };
   }
 
@@ -46,10 +46,11 @@ export default class StoryDetails extends React.Component {
 					     finished: doc.data().finishedOn !== undefined,
 					     finishedOn: doc.data().finishedOn !== undefined ? doc.data().finishedOn.seconds : undefined,
 					     interruptions: doc.data().interruptions !== undefined ? doc.data().interruptions : [],
+							 interruptionCategories: doc.data().interruptionCategories !== undefined ? doc.data().interruptionCategories : [],
 					   })
 	  });
   }
-  
+
   secondsDifferenceAsString(start, finish) {
 	  difference = finish - start;
 	  seconds = difference % 60;
@@ -62,7 +63,7 @@ export default class StoryDetails extends React.Component {
 	  difference -= hours;
 	  difference /= 24;
 	  days = difference;
-	  
+
 	  result = '';
 	  if (days > 0) {
 		  result += days + 'd';
@@ -75,27 +76,33 @@ export default class StoryDetails extends React.Component {
 		  if (result !== '') {result += ' '}
 		  result += minutes + 'm';
 	  }
-	  
+
 	  if (result == '') {
 		  result += ((seconds - (seconds % 1))) + 's';
 	  }
-	  
+
 	  return result;
   }
-  
+
   dateAsString(seconds) {
 	  var asDate = new Date(seconds*1000);
 	  return asDate.toDateString() + ' ' + asDate.toLocaleTimeString();
   }
 
-  addInterrupt = () => {
+  addInterrupt = (category) => {
 	  let newInterrupts = this.state.interruptions
 	  newInterrupts.push(new Date());
-	  this.props.navigation.getParam('story').ref.update({
-		    interruptions: newInterrupts
+		let newInterruptCats = this.state.interruptionCategories
+		if (category !== undefined) {
+			newInterruptCats.push(category);
+		}
+
+		this.props.navigation.getParam('story').ref.update({
+				interruptions: newInterrupts,
+				interruptionCategories: newInterruptCats
 		});
   }
-  
+
   addStartedOn = () => {
 	  this.props.navigation.getParam('story').ref.update({
 		    startedOn: new Date()
@@ -113,6 +120,31 @@ export default class StoryDetails extends React.Component {
 	  this.addFinishedOn();
   }
 
+	iconColor = (interruptionCategory) => {
+		switch (interruptionCategory) {
+      case 'meeting':
+        return {color: 'purple',};
+			case 'waiting':
+        return {color: 'orange',};
+      case 'other':
+        return {color: 'blue',};
+      default:
+        return {color: 'black',};
+    }
+	}
+	iconName = (interruptionCategory) => {
+		switch (interruptionCategory) {
+      case 'meeting':
+        return 'chatbubbles';
+			case 'waiting':
+        return 'people';
+      case 'other':
+        return 'hand';
+      default:
+        return 'question';
+    }
+	}
+
   convertInterruptionTimesToIntervals() {
 	  var result = [];
 	  var i;
@@ -121,67 +153,93 @@ export default class StoryDetails extends React.Component {
 	  for (i = 0; i + 1 < this.state.interruptions.length; i += 2) {
 		  first = this.state.interruptions[i].seconds;
 		  second = this.state.interruptions[i+1].seconds;
-		  result.push('Interrupt ' + ((i/2)+1) + ': ' + new Date(first*1000).toDateString().slice(4) + ' - ' + new Date(second*1000).toDateString().slice(4) + ' took ' + this.secondsDifferenceAsString(first, second));
+		  result.push({
+				interruptTime: this.secondsDifferenceAsString(first, second),
+				interruptStart: new Date(first*1000).toLocaleTimeString(),
+				iconColor: this.iconColor(this.state.interruptionCategories[i/2]),
+				iconName: this.iconName(this.state.interruptionCategories[i/2])
+			});
 	  }
 	  if (this.state.interruptions.length % 2 == 1) {
 		  var current = this.state.interruptions[this.state.interruptions.length - 1].seconds;
-		  result.push('Currently interrupted, started at ' + new Date(current*1000).toDateString().slice(4));
+		  result.push({
+				displayText: 'Currently interrupted, started at ' + new Date(current*1000).toDateString().slice(4),
+				iconColor: this.iconColor(this.state.interruptionCategories[i/2]),
+				iconName: this.iconName(this.state.interruptionCategories[i/2])
+			});
 	  }
 	  return result;
   }
-  
+
   render() {
 	let interruptionList;
 	if (this.state.interruptions.length !== 0) {
 		// Add all the finished interruptions in reverse order
-		_keyExtractor = (item, index) => index + ' ' + item.seconds;        
-		interruptionList =           
+		_keyExtractor = (item, index) => index + ' ' + item.seconds;
+		interruptionList =
         	<FlatList
         	  data={this.convertInterruptionTimesToIntervals()}
 		 	  extraData={this.state}
 		      keyExtractor={_keyExtractor}
-        	  renderItem={({item}) => <Text>{item}</Text>}
+        	  renderItem={({item}) =>
+							<View style={styles.interruptionItem}>
+								<Icon active style={item.iconColor} name={item.iconName} />
+								<Text>&nbsp;&nbsp;At&nbsp;{item.interruptStart},&nbsp;lasted&nbsp;<Text style={{fontWeight: 'bold'}}>{item.interruptTime}</Text></Text>
+							</View>
+						}
         	/>;
 	}
-	
+
 	let element;
 	if (!this.state.started) {
-		element = 
+		element =
 		   <Button onPress={this.addStartedOn}>
 		     <Text>Start story</Text>
 		   </Button>;
 	} else if (!this.state.finished) {
 		if (this.state.interruptions.length % 2 == 0) {
-			element = 
+			element =
 				 <View>
 					<Text>Started on {this.dateAsString(this.state.startedOn)}</Text>
 					{interruptionList}
-					<View style={{flexDirection: 'row', justifyContent: 'space-between', }}>
-					   <Button onPress={this.addInterrupt}>
-						 <Text>Interrupt Progress</Text>
+					<View style={styles.buttonContainer}>
+					   <Button style={{backgroundColor: 'purple'}} onPress={() => this.addInterrupt('meeting')}>
+							 <Icon active name="chatbubbles" />
+							 <Text>Meeting</Text>
 					   </Button>
-					   <Button onPress={this.addFinishedOn}>
-						 <Text>Finish story</Text>
+						 <Button style={{backgroundColor: 'orange'}} onPress={() => this.addInterrupt('waiting')}>
+							 <Icon active name="people" />
+							 <Text>Waiting on others</Text>
+					   </Button>
+						 <Button style={{backgroundColor: 'blue'}} onPress={() => this.addInterrupt('other')}>
+							 <Icon active name="hand" />
+							 <Text>Other</Text>
+					   </Button>
+					   <Button style={styles.finishButton} onPress={this.addFinishedOn}>
+							 <Icon active name="checkmark" />
+							 <Text>Finish story</Text>
 					   </Button>
 					</View>
 				 </View>;
 		} else {
-			element = 
+			element =
 				 <View>
 				   <Text>Started on {this.dateAsString(this.state.startedOn)}</Text>
 					{interruptionList}
-					<View style={{flexDirection: 'row', justifyContent: 'space-between', }}>
-					   <Button onPress={this.addInterrupt}>
-						 <Text>Resume Progress</Text>
+					<View style={styles.buttonContainer}>
+					   <Button onPress={() => this.addInterrupt(undefined)}>
+							 <Icon active name="refresh" />
+							 <Text>Resume Progress</Text>
 					   </Button>
-					   <Button onPress={this.finishInterruptedStory}>
-						 <Text>Finish story</Text>
+					   <Button style={styles.finishButton} onPress={this.finishInterruptedStory}>
+							 <Icon active name="checkmark" />
+							 <Text>Finish story</Text>
 					   </Button>
 					</View>
 				 </View>;
 		}
 	} else {
-		element = 
+		element =
 	    <View>
 		  <Text>Started on {this.dateAsString(this.state.startedOn)}</Text>
 		  <Text>Finished on {this.dateAsString(this.state.finishedOn)}</Text>
@@ -198,3 +256,22 @@ export default class StoryDetails extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+  buttonContainer: {
+		flexDirection: 'column',
+		justifyContent: 'flex-end',
+	},
+	finishButton: {
+		backgroundColor: 'green',
+	},
+	interruptionItem: {
+		flexDirection: 'row',
+		padding: 5,
+	}
+});
