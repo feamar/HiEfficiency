3210138276945b2dc023085070f573e339904409
Teun Kooijman <teun.kooijman@gmail.com>
2018-09-27 16:37:21 +0200

    TypeScript - Added Routing Types for global convenience.

--- app/components/routing/RoutingTypes.ts
+++ app/components/routing/RoutingTypes.ts
@@ -1,14 +1,24 @@
 import { NavigationRoute } from "react-navigation";
 
+
+//Dn't make it extend NavigationScreenProps
+export interface CustomScreenProps
+{
+
+}
+
+
 export interface CustomNavigationState
 {
     routes: Array<NavigationRoute>
 }
   
+//Don't make it extend NavigationParam, because that simply makes it indexable on string keys (i.e. allows any, and escapes from the type system.)
 export interface CustomNavigationParams
 {
   header_right_injection?: () => JSX.Element,
   initial_route_name?: string,
   title: string,
-  subtitle?: string
+  subtitle?: string,
+  onBackClicked?: () => boolean
 }
